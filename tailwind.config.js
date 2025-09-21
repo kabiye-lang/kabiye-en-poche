404ef0a1545a59178c0aef8f81267f5611816cc2
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2025-09-21 15:28:41 +0100

    fix: upgrade deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/app/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}',
-    './src/screens/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class', // Enable dark mode with class strategy
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
