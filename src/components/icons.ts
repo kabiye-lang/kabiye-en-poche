42feafa25cdd520dcaa730fade84c45cbf81c47a
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2026-02-02 19:59:37 +0100

    fix: cleanup icons

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -1,113 +1,30 @@
-import { AirplaneIcon as PhosphorAirplaneIcon } from 'phosphor-react-native/src/icons/Airplane'
-import { AppleLogoIcon as PhosphorAppleLogoIcon } from 'phosphor-react-native/src/icons/AppleLogo'
-import { ArrowClockwiseIcon as PhosphorArrowClockwiseIcon } from 'phosphor-react-native/src/icons/ArrowClockwise'
-import { ArrowDownIcon as PhosphorArrowDownIcon } from 'phosphor-react-native/src/icons/ArrowDown'
 import { ArrowFatLinesUpIcon as PhosphorArrowFatLinesUpIcon } from 'phosphor-react-native/src/icons/ArrowFatLinesUp'
 import { ArrowFatLineUpIcon as PhosphorArrowFatLineUpIcon } from 'phosphor-react-native/src/icons/ArrowFatLineUp'
-import { ArrowLeftIcon as PhosphorArrowLeftIcon } from 'phosphor-react-native/src/icons/ArrowLeft'
 import { ArrowRightIcon as PhosphorArrowRightIcon } from 'phosphor-react-native/src/icons/ArrowRight'
 import { ArrowsClockwiseIcon as PhosphorArrowsClockwiseIcon } from 'phosphor-react-native/src/icons/ArrowsClockwise'
-import { ArrowUpIcon as PhosphorArrowUpIcon } from 'phosphor-react-native/src/icons/ArrowUp'
 import { ArticleIcon as PhosphorArticleIcon } from 'phosphor-react-native/src/icons/Article'
 import { BackspaceIcon as PhosphorBackspaceIcon } from 'phosphor-react-native/src/icons/Backspace'
-import { BellIcon as PhosphorBellIcon } from 'phosphor-react-native/src/icons/Bell'
-import { BicycleIcon as PhosphorBicycleIcon } from 'phosphor-react-native/src/icons/Bicycle'
-import { BookmarkIcon as PhosphorBookmarkIcon } from 'phosphor-react-native/src/icons/Bookmark'
-import { BookmarksIcon as PhosphorBookmarksIcon } from 'phosphor-react-native/src/icons/Bookmarks'
 import { BookOpenTextIcon as PhosphorBookOpenTextIcon } from 'phosphor-react-native/src/icons/BookOpenText'
-import { BugIcon as PhosphorBugIcon } from 'phosphor-react-native/src/icons/Bug'
-import { CalendarIcon as PhosphorCalendarIcon } from 'phosphor-react-native/src/icons/Calendar'
-import { CameraIcon as PhosphorCameraIcon } from 'phosphor-react-native/src/icons/Camera'
-import { CarIcon as PhosphorCarIcon } from 'phosphor-react-native/src/icons/Car'
-import { CaretDownIcon as PhosphorCaretDownIcon } from 'phosphor-react-native/src/icons/CaretDown'
 import { CaretLeftIcon as PhosphorCaretLeftIcon } from 'phosphor-react-native/src/icons/CaretLeft'
 import { CaretRightIcon as PhosphorCaretRightIcon } from 'phosphor-react-native/src/icons/CaretRight'
-import { CaretUpIcon as PhosphorCaretUpIcon } from 'phosphor-react-native/src/icons/CaretUp'
-import { ChatIcon as PhosphorChatIcon } from 'phosphor-react-native/src/icons/Chat'
-import { ChatCircleIcon as PhosphorChatCircleIcon } from 'phosphor-react-native/src/icons/ChatCircle'
-import { ChatCircleDotsIcon as PhosphorChatCircleDotsIcon } from 'phosphor-react-native/src/icons/ChatCircleDots'
-import { ChatsTeardropIcon as PhosphorChatsTeardropIcon } from 'phosphor-react-native/src/icons/ChatsTeardrop'
-import { CheckIcon as PhosphorCheckIcon } from 'phosphor-react-native/src/icons/Check'
 import { CheckCircleIcon as PhosphorCheckCircleIcon } from 'phosphor-react-native/src/icons/CheckCircle'
-import { ClockIcon as PhosphorClockIcon } from 'phosphor-react-native/src/icons/Clock'
-import { CloudIcon as PhosphorCloudIcon } from 'phosphor-react-native/src/icons/Cloud'
-import { CoffeeIcon as PhosphorCoffeeIcon } from 'phosphor-react-native/src/icons/Coffee'
-import { ConfettiIcon as PhosphorConfettiIcon } from 'phosphor-react-native/src/icons/Confetti'
-import { CreditCardIcon as PhosphorCreditCardIcon } from 'phosphor-react-native/src/icons/CreditCard'
-import { CrosshairIcon as PhosphorCrosshairIcon } from 'phosphor-react-native/src/icons/Crosshair'
-import { CurrencyDollarIcon as PhosphorCurrencyDollarIcon } from 'phosphor-react-native/src/icons/CurrencyDollar'
-import { DatabaseIcon as PhosphorDatabaseIcon } from 'phosphor-react-native/src/icons/Database'
 import { DotIcon as PhosphorDotIcon } from 'phosphor-react-native/src/icons/Dot'
-import { DotsThreeIcon as PhosphorDotsThreeIcon } from 'phosphor-react-native/src/icons/DotsThree'
-import { DownloadIcon as PhosphorDownloadIcon } from 'phosphor-react-native/src/icons/Download'
-import { EnvelopeIcon as PhosphorEnvelopeIcon } from 'phosphor-react-native/src/icons/Envelope'
-import { EyeIcon as PhosphorEyeIcon } from 'phosphor-react-native/src/icons/Eye'
-import { EyeSlashIcon as PhosphorEyeSlashIcon } from 'phosphor-react-native/src/icons/EyeSlash'
-import { FacebookLogoIcon as PhosphorFacebookLogoIcon } from 'phosphor-react-native/src/icons/FacebookLogo'
 import { FilePdfIcon as PhosphorFilePdfIcon } from 'phosphor-react-native/src/icons/FilePdf'
-import { FileTextIcon as PhosphorFileTextIcon } from 'phosphor-react-native/src/icons/FileText'
-import { FirstAidIcon as PhosphorFirstAidIcon } from 'phosphor-react-native/src/icons/FirstAid'
-import { FolderOpenIcon as PhosphorFolderOpenIcon } from 'phosphor-react-native/src/icons/FolderOpen'
-import { FunnelIcon as PhosphorFunnelIcon } from 'phosphor-react-native/src/icons/Funnel'
-import { GameControllerIcon as PhosphorGameControllerIcon } from 'phosphor-react-native/src/icons/GameController'
 import { GearIcon as PhosphorGearIcon } from 'phosphor-react-native/src/icons/Gear'
 import { GlobeIcon as PhosphorGlobeIcon } from 'phosphor-react-native/src/icons/Globe'
-import { GlobeSimpleIcon as PhosphorGlobeSimpleIcon } from 'phosphor-react-native/src/icons/GlobeSimple'
-import { GoogleLogoIcon as PhosphorGoogleLogoIcon } from 'phosphor-react-native/src/icons/GoogleLogo'
-import { HandshakeIcon as PhosphorHandshakeIcon } from 'phosphor-react-native/src/icons/Handshake'
-import { HeartIcon as PhosphorHeartIcon } from 'phosphor-react-native/src/icons/Heart'
 import { HouseIcon as PhosphorHouseIcon } from 'phosphor-react-native/src/icons/House'
-import { IdentificationCardIcon as PhosphorIdentificationCardIcon } from 'phosphor-react-native/src/icons/IdentificationCard'
-import { ImageIcon as PhosphorImageIcon } from 'phosphor-react-native/src/icons/Image'
-import { InfoIcon as PhosphorInfoIcon } from 'phosphor-react-native/src/icons/Info'
 import { KeyboardIcon as PhosphorKeyboardIcon } from 'phosphor-react-native/src/icons/Keyboard'
 import { KeyReturnIcon as PhosphorKeyReturnIcon } from 'phosphor-react-native/src/icons/KeyReturn'
 import { LightbulbIcon as PhosphorLightbulbIcon } from 'phosphor-react-native/src/icons/Lightbulb'
-import { LightningIcon as PhosphorLightningIcon } from 'phosphor-react-native/src/icons/Lightning'
-import { ListIcon as PhosphorListIcon } from 'phosphor-react-native/src/icons/List'
-import { ListBulletsIcon as PhosphorListBulletsIcon } from 'phosphor-react-native/src/icons/ListBullets'
 import { LockIcon as PhosphorLockIcon } from 'phosphor-react-native/src/icons/Lock'
 import { MagnifyingGlassIcon as PhosphorMagnifyingGlassIcon } from 'phosphor-react-native/src/icons/MagnifyingGlass'
-import { MapPinIcon as PhosphorMapPinIcon } from 'phosphor-react-native/src/icons/MapPin'
-import { MinusIcon as PhosphorMinusIcon } from 'phosphor-react-native/src/icons/Minus'
-import { MoonIcon as PhosphorMoonIcon } from 'phosphor-react-native/src/icons/Moon'
-import { PackageIcon as PhosphorPackageIcon } from 'phosphor-react-native/src/icons/Package'
-import { PaletteIcon as PhosphorPaletteIcon } from 'phosphor-react-native/src/icons/Palette'
-import { PaperPlaneIcon as PhosphorPaperPlaneIcon } from 'phosphor-react-native/src/icons/PaperPlane'
-import { PaperPlaneTiltIcon as PhosphorPaperPlaneTiltIcon } from 'phosphor-react-native/src/icons/PaperPlaneTilt'
-import { PauseIcon as PhosphorPauseIcon } from 'phosphor-react-native/src/icons/Pause'
-import { PencilIcon as PhosphorPencilIcon } from 'phosphor-react-native/src/icons/Pencil'
-import { PhoneIcon as PhosphorPhoneIcon } from 'phosphor-react-native/src/icons/Phone'
 import { PlayIcon as PhosphorPlayIcon } from 'phosphor-react-native/src/icons/Play'
-import { PlusIcon as PhosphorPlusIcon } from 'phosphor-react-native/src/icons/Plus'
-import { QuestionIcon as PhosphorQuestionIcon } from 'phosphor-react-native/src/icons/Question'
-import { ScalesIcon as PhosphorScalesIcon } from 'phosphor-react-native/src/icons/Scales'
-import { ShareIcon as PhosphorShareIcon } from 'phosphor-react-native/src/icons/Share'
-import { ShieldIcon as PhosphorShieldIcon } from 'phosphor-react-native/src/icons/Shield'
-import { ShieldCheckIcon as PhosphorShieldCheckIcon } from 'phosphor-react-native/src/icons/ShieldCheck'
-import { ShoppingBagIcon as PhosphorShoppingBagIcon } from 'phosphor-react-native/src/icons/ShoppingBag'
-import { ShoppingCartIcon as PhosphorShoppingCartIcon } from 'phosphor-react-native/src/icons/ShoppingCart'
-import { SignOutIcon as PhosphorSignOutIcon } from 'phosphor-react-native/src/icons/SignOut'
 import { SparkleIcon as PhosphorSparkleIcon } from 'phosphor-react-native/src/icons/Sparkle'
 import { SpeakerHighIcon as PhosphorSpeakerHighIcon } from 'phosphor-react-native/src/icons/SpeakerHigh'
 import { SpeakerSlashIcon as PhosphorSpeakerSlashIcon } from 'phosphor-react-native/src/icons/SpeakerSlash'
-import { StarIcon as PhosphorStarIcon } from 'phosphor-react-native/src/icons/Star'
-import { SunIcon as PhosphorSunIcon } from 'phosphor-react-native/src/icons/Sun'
-import { TagIcon as PhosphorTagIcon } from 'phosphor-react-native/src/icons/Tag'
-import { ThumbsDownIcon as PhosphorThumbsDownIcon } from 'phosphor-react-native/src/icons/ThumbsDown'
-import { ThumbsUpIcon as PhosphorThumbsUpIcon } from 'phosphor-react-native/src/icons/ThumbsUp'
-import { TrainIcon as PhosphorTrainIcon } from 'phosphor-react-native/src/icons/Train'
 import { TrashIcon as PhosphorTrashIcon } from 'phosphor-react-native/src/icons/Trash'
-import { TrendUpIcon as PhosphorTrendUpIcon } from 'phosphor-react-native/src/icons/TrendUp'
-import { TruckIcon as PhosphorTruckIcon } from 'phosphor-react-native/src/icons/Truck'
-import { UploadIcon as PhosphorUploadIcon } from 'phosphor-react-native/src/icons/Upload'
 import { UserIcon as PhosphorUserIcon } from 'phosphor-react-native/src/icons/User'
-import { UserCircleIcon as PhosphorUserCircleIcon } from 'phosphor-react-native/src/icons/UserCircle'
-import { UserPlusIcon as PhosphorUserPlusIcon } from 'phosphor-react-native/src/icons/UserPlus'
-import { WarningIcon as PhosphorWarningIcon } from 'phosphor-react-native/src/icons/Warning'
-import { WarningCircleIcon as PhosphorWarningCircleIcon } from 'phosphor-react-native/src/icons/WarningCircle'
 import { XIcon as PhosphorXIcon } from 'phosphor-react-native/src/icons/X'
-import { XCircleIcon as PhosphorXCircleIcon } from 'phosphor-react-native/src/icons/XCircle'
 import { withUniwind } from 'uniwind'
 
 /**
@@ -115,113 +32,30 @@ import { withUniwind } from 'uniwind'
  * Uniwind automatically maps 'color' prop to 'colorClassName'
  */
 
-export const AirplaneIcon = withUniwind(PhosphorAirplaneIcon)
-export const AppleLogoIcon = withUniwind(PhosphorAppleLogoIcon)
-export const ArrowClockwiseIcon = withUniwind(PhosphorArrowClockwiseIcon)
-export const ArrowDownIcon = withUniwind(PhosphorArrowDownIcon)
+export const ArticleIcon = withUniwind(PhosphorArticleIcon)
 export const ArrowFatLinesUpIcon = withUniwind(PhosphorArrowFatLinesUpIcon)
 export const ArrowFatLineUpIcon = withUniwind(PhosphorArrowFatLineUpIcon)
-export const ArrowLeftIcon = withUniwind(PhosphorArrowLeftIcon)
 export const ArrowRightIcon = withUniwind(PhosphorArrowRightIcon)
 export const ArrowsClockwiseIcon = withUniwind(PhosphorArrowsClockwiseIcon)
-export const ArrowUpIcon = withUniwind(PhosphorArrowUpIcon)
-export const ArticleIcon = withUniwind(PhosphorArticleIcon)
 export const BackspaceIcon = withUniwind(PhosphorBackspaceIcon)
-export const BellIcon = withUniwind(PhosphorBellIcon)
-export const BicycleIcon = withUniwind(PhosphorBicycleIcon)
-export const BookmarkIcon = withUniwind(PhosphorBookmarkIcon)
-export const BookmarksIcon = withUniwind(PhosphorBookmarksIcon)
 export const BookOpenTextIcon = withUniwind(PhosphorBookOpenTextIcon)
-export const BugIcon = withUniwind(PhosphorBugIcon)
-export const CalendarIcon = withUniwind(PhosphorCalendarIcon)
-export const CameraIcon = withUniwind(PhosphorCameraIcon)
-export const CarIcon = withUniwind(PhosphorCarIcon)
-export const CaretDownIcon = withUniwind(PhosphorCaretDownIcon)
 export const CaretLeftIcon = withUniwind(PhosphorCaretLeftIcon)
 export const CaretRightIcon = withUniwind(PhosphorCaretRightIcon)
-export const CaretUpIcon = withUniwind(PhosphorCaretUpIcon)
-export const ChatIcon = withUniwind(PhosphorChatIcon)
-export const ChatCircleIcon = withUniwind(PhosphorChatCircleIcon)
-export const ChatCircleDotsIcon = withUniwind(PhosphorChatCircleDotsIcon)
-export const ChatsTeardropIcon = withUniwind(PhosphorChatsTeardropIcon)
-export const CheckIcon = withUniwind(PhosphorCheckIcon)
 export const CheckCircleIcon = withUniwind(PhosphorCheckCircleIcon)
-export const ClockIcon = withUniwind(PhosphorClockIcon)
-export const CloudIcon = withUniwind(PhosphorCloudIcon)
-export const CoffeeIcon = withUniwind(PhosphorCoffeeIcon)
-export const ConfettiIcon = withUniwind(PhosphorConfettiIcon)
-export const CreditCardIcon = withUniwind(PhosphorCreditCardIcon)
-export const CrosshairIcon = withUniwind(PhosphorCrosshairIcon)
-export const CurrencyDollarIcon = withUniwind(PhosphorCurrencyDollarIcon)
-export const DatabaseIcon = withUniwind(PhosphorDatabaseIcon)
 export const DotIcon = withUniwind(PhosphorDotIcon)
-export const DotsThreeIcon = withUniwind(PhosphorDotsThreeIcon)
-export const DownloadIcon = withUniwind(PhosphorDownloadIcon)
-export const EnvelopeIcon = withUniwind(PhosphorEnvelopeIcon)
-export const EyeIcon = withUniwind(PhosphorEyeIcon)
-export const EyeSlashIcon = withUniwind(PhosphorEyeSlashIcon)
-export const FacebookLogoIcon = withUniwind(PhosphorFacebookLogoIcon)
 export const FilePdfIcon = withUniwind(PhosphorFilePdfIcon)
-export const FileTextIcon = withUniwind(PhosphorFileTextIcon)
-export const FirstAidIcon = withUniwind(PhosphorFirstAidIcon)
-export const FolderOpenIcon = withUniwind(PhosphorFolderOpenIcon)
-export const FunnelIcon = withUniwind(PhosphorFunnelIcon)
-export const GameControllerIcon = withUniwind(PhosphorGameControllerIcon)
 export const GearIcon = withUniwind(PhosphorGearIcon)
 export const GlobeIcon = withUniwind(PhosphorGlobeIcon)
-export const GlobeSimpleIcon = withUniwind(PhosphorGlobeSimpleIcon)
-export const GoogleLogoIcon = withUniwind(PhosphorGoogleLogoIcon)
-export const HandshakeIcon = withUniwind(PhosphorHandshakeIcon)
-export const HeartIcon = withUniwind(PhosphorHeartIcon)
 export const HouseIcon = withUniwind(PhosphorHouseIcon)
-export const IdentificationCardIcon = withUniwind(PhosphorIdentificationCardIcon)
-export const ImageIcon = withUniwind(PhosphorImageIcon)
-export const InfoIcon = withUniwind(PhosphorInfoIcon)
 export const KeyboardIcon = withUniwind(PhosphorKeyboardIcon)
 export const KeyReturnIcon = withUniwind(PhosphorKeyReturnIcon)
 export const LightbulbIcon = withUniwind(PhosphorLightbulbIcon)
-export const LightningIcon = withUniwind(PhosphorLightningIcon)
-export const ListIcon = withUniwind(PhosphorListIcon)
-export const ListBulletsIcon = withUniwind(PhosphorListBulletsIcon)
 export const LockIcon = withUniwind(PhosphorLockIcon)
 export const MagnifyingGlassIcon = withUniwind(PhosphorMagnifyingGlassIcon)
-export const MapPinIcon = withUniwind(PhosphorMapPinIcon)
-export const MinusIcon = withUniwind(PhosphorMinusIcon)
-export const MoonIcon = withUniwind(PhosphorMoonIcon)
-export const PackageIcon = withUniwind(PhosphorPackageIcon)
-export const PaletteIcon = withUniwind(PhosphorPaletteIcon)
-export const PaperPlaneIcon = withUniwind(PhosphorPaperPlaneIcon)
-export const PaperPlaneTiltIcon = withUniwind(PhosphorPaperPlaneTiltIcon)
-export const PauseIcon = withUniwind(PhosphorPauseIcon)
-export const PencilIcon = withUniwind(PhosphorPencilIcon)
-export const PhoneIcon = withUniwind(PhosphorPhoneIcon)
 export const PlayIcon = withUniwind(PhosphorPlayIcon)
-export const PlusIcon = withUniwind(PhosphorPlusIcon)
-export const QuestionIcon = withUniwind(PhosphorQuestionIcon)
-export const ScalesIcon = withUniwind(PhosphorScalesIcon)
-export const ShareIcon = withUniwind(PhosphorShareIcon)
-export const ShieldIcon = withUniwind(PhosphorShieldIcon)
-export const ShieldCheckIcon = withUniwind(PhosphorShieldCheckIcon)
-export const ShoppingBagIcon = withUniwind(PhosphorShoppingBagIcon)
-export const ShoppingCartIcon = withUniwind(PhosphorShoppingCartIcon)
-export const SignOutIcon = withUniwind(PhosphorSignOutIcon)
 export const SparkleIcon = withUniwind(PhosphorSparkleIcon)
 export const SpeakerHighIcon = withUniwind(PhosphorSpeakerHighIcon)
 export const SpeakerSlashIcon = withUniwind(PhosphorSpeakerSlashIcon)
-export const StarIcon = withUniwind(PhosphorStarIcon)
-export const SunIcon = withUniwind(PhosphorSunIcon)
-export const TagIcon = withUniwind(PhosphorTagIcon)
-export const ThumbsDownIcon = withUniwind(PhosphorThumbsDownIcon)
-export const ThumbsUpIcon = withUniwind(PhosphorThumbsUpIcon)
-export const TrainIcon = withUniwind(PhosphorTrainIcon)
 export const TrashIcon = withUniwind(PhosphorTrashIcon)
-export const TrendUpIcon = withUniwind(PhosphorTrendUpIcon)
-export const TruckIcon = withUniwind(PhosphorTruckIcon)
-export const UploadIcon = withUniwind(PhosphorUploadIcon)
 export const UserIcon = withUniwind(PhosphorUserIcon)
-export const UserCircleIcon = withUniwind(PhosphorUserCircleIcon)
-export const UserPlusIcon = withUniwind(PhosphorUserPlusIcon)
-export const WarningIcon = withUniwind(PhosphorWarningIcon)
-export const WarningCircleIcon = withUniwind(PhosphorWarningCircleIcon)
 export const XIcon = withUniwind(PhosphorXIcon)
-export const XCircleIcon = withUniwind(PhosphorXCircleIcon)
