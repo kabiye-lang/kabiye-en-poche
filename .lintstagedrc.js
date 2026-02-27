fae97a2b8fac2dfa57426b55e18998df8bbbd722
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2026-02-27 09:07:43 +0000

    chore: update lint-staged configuration for admin and website directories

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,17 @@ const path = require('path')
 const buildEslintCommand = (filenames) =>
   `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
 
+const buildAdminEslintCommand = (filenames) =>
+  `cd admin && npx eslint --fix ${filenames.map((f) => path.relative('admin', f)).join(' ')}`
+
+const buildWebsiteEslintCommand = (filenames) =>
+  `cd website && npx eslint --fix ${filenames.map((f) => path.relative('website', f)).join(' ')}`
+
 module.exports = {
+  'admin/**/*.{js,jsx,ts,tsx}': [buildAdminEslintCommand],
+  'website/**/*.{js,jsx,ts,tsx}': [buildWebsiteEslintCommand],
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '!(admin|website)/**/*.{js,jsx,ts,tsx}': [buildEslintCommand],
   /**
    * Check this issues
    * https://github.com/okonet/lint-staged/issues/825
