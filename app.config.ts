1e27da32862bceba480f78106051946112e9d59e
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2026-04-06 15:36:27 +0100

    feat: add fmtfix plugin to resolve consteval compilation error in Xcode

--- app.config.ts
+++ app.config.ts
@@ -102,6 +102,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     'expo-web-browser',
     'expo-audio',
     'expo-secure-store',
+    './fmtfix.js',
   ],
   experiments: {
     tsconfigPaths: true,
