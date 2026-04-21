0ea0211d6d5c5fa0f0327bad3af5a51220f53fe1
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2026-04-21 07:43:58 +0100

    feat: add @rolldown/plugin-babel to enhance Babel integration in Vite configuration

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 import path from "path";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [basicSsl(), react({
-    babel: {
-      plugins: ["babel-plugin-react-compiler"],
-    },
-  }), cloudflare()],
+  plugins: [
+    basicSsl(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    cloudflare(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
